6bc169152d3335c198059b24d85300fbce014b17
Azad Bin Johari <azadjohari@gmail.com>
2021-04-01 22:29:27 +0800

    2229

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -2,8 +2,4 @@ function transformUppercase(_val){
     return _val.toUpperCase();
 }
 
-var elNamaPengguna = document.getElementById('namaPengguna');
-elNamaPengguna.addEventListener("keyup", function(){
-    elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
-});
-//fdf
+var a = document.getElementById("namaPengguna")
