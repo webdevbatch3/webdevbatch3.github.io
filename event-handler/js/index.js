19c70922d7b081f39a5d0e59248e4d3a91226e39
webdevbatch3 <81572522+webdevbatch3@users.noreply.github.com>
2021-04-01 22:26:03 +0800

    2026

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -2,7 +2,7 @@ function transformUppercase(_val){
     return _val.toUpperCase();
 }
 
-var elNamaPengguna = document.getElementById("namaPengguna");
+var elNamaPengguna = document.getElementById('namaPengguna');
 elNamaPengguna.addEventListener("keyup", function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
