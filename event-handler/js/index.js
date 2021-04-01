43505fc9e6645a04b066df1110e294a37dbb3954
Azad Bin Johari <azadjohari@gmail.com>
2021-04-01 22:32:43 +0800

    letak script before end html tag

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -2,4 +2,7 @@ function transformUppercase(_val){
     return _val.toUpperCase();
 }
 
-var a = document.getElementById("namaPengguna")
+let elNamaPengguna = document.getElementById("namaPengguna");
+elNamaPengguna.addEventListener("keyup",function(){
+    elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
+});
