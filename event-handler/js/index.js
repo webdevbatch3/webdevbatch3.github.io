5f39905c0fc3d3ec6a2f19de655b276cdebac4d5
Azad Bin Johari <azadjohari@gmail.com>
2021-04-02 19:39:05 +0800

    1938

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -6,3 +6,16 @@ let elNamaPengguna = document.getElementById("namaPengguna"); //1) select the el
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan");
+    let password_val = elKataLaluan.value;
+    if(password_val.length > 5){
+        alert("Password Check...OK!");
+    } else {
+        alert("Password too short!");
+    }
+}
+
+let elKataLaluan = document.getElementById("kataLaluan");
+elKataLaluan.onblur = checkPassword;
