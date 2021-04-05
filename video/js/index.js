978f5ade87e01cd1aba426988aa032c3d778b14a
Azad Bin Johari <azadjohari@gmail.com>
2021-04-05 20:45:45 +0800

    2045

--- video/js/index.js
+++ video/js/index.js
@@ -4,3 +4,8 @@ let playBtn = document.getElementById("playBtn");
 playBtn.addEventListener("click", function(){
     video.play();
 });
+
+let pauseBtn = document.getElementById("pauseBtn");
+pauseBtn.addEventListener("click",function(){
+    video.pause();
+});
