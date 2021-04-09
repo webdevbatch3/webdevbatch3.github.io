3f8c9781b29e9ea346d24a485e45203795823218
Azad Bin Johari <azadjohari@gmail.com>
2021-04-09 21:47:55 +0800

    2147

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -11,6 +11,11 @@ function GetBooking(){
 
         let bookingNameList = document.getElementById("bookingNameList");
 
+        //clear the table rows
+        for(let k = bookingNameList.rows.length - 1; k > 0; k--){
+            bookingNameList.deleteRow(k);
+        }
+
         for(let i = 0; i < json.bookingitems.length; i++){
             let gName = json.bookingitems[i].name;
             let gEmail = json.bookingitems[i].email;
