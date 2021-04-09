161d58c1cd5519f418b91f2e7268c090c607a688
Azad Bin Johari <azadjohari@gmail.com>
2021-04-09 21:41:32 +0800

    2141

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -1,4 +1,30 @@
 let refreshNowBtn = document.getElementById("refreshNow");
 refreshNowBtn.addEventListener("click",function(){
-    alert("GetBooking");
+    GetBooking();
 });
+
+function GetBooking(){
+    let url = 'https://api.sheety.co/b9b23bacbce0fa05289abc34d8cf52e6/tableapp/bookingitems';
+    fetch(url)
+    .then((response) => response.json())
+    .then(json => {
+
+        let bookingNameList = document.getElementById("bookingNameList");
+
+        for(let i = 0; i < json.bookingitems.length; i++){
+            let gName = json.bookingitems[i].name;
+            let gEmail = json.bookingitems[i].email;
+            let gPax = json.bookingitems[i].pax;
+            let gRemarks = json.bookingitems[i].remarks;
+            let gId = json.bookingitems[i].id;
+
+            let row = bookingNameList.insertRow(bookingNameList.rows.length);
+            row.insertCell(0).innerHTML = gId;
+            row.insertCell(1).innerHTML = gName;
+            row.insertCell(2).innerHTML = gEmail;
+            row.insertCell(3).innerHTML = gPax;
+            row.insertCell(4).innerHTML = gRemarks;
+            row.insertCell(5).innerHTML = "TBC";
+        }
+    });
+}
