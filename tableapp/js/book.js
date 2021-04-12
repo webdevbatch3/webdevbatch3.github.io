166e7735782ef8534852b4a64147ed74ce827d09
Azad Bin Johari <azadjohari@gmail.com>
2021-04-12 20:35:50 +0800

    2035

--- tableapp/js/book.js
+++ tableapp/js/book.js
@@ -31,6 +31,6 @@ function BookNow(userName, userEmail, userPax, userRemarks) {
     })
         .then((response) => response.json())
         .then(json => {
-            alert(json.bookingitem.name + " successfully added!");
+            alert("ID:" + json.bookingitem.id + " " + json.bookingitem.name + " successfully added!");
         });
 }
