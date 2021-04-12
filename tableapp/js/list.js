9ec45122555e84a76c5e48356a7aa820f475311d
Azad Bin Johari <azadjohari@gmail.com>
2021-04-12 21:30:04 +0800

    2129

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -29,7 +29,7 @@ function GetBooking(){
             row.insertCell(2).innerHTML = gEmail;
             row.insertCell(3).innerHTML = gPax;
             row.insertCell(4).innerHTML = gRemarks;
-            row.insertCell(5).innerHTML = "TBC";
+            row.insertCell(5).innerHTML = "<button id='" + gId +"' class='btn btn-danger'>Delete</button>";
         }
     });
 }
