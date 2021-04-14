6aed0102c3f784af13d216675b13321860c646c7
Azad Bin Johari <azadjohari@gmail.com>
2021-04-14 20:15:48 +0800

    2015

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -21,7 +21,8 @@ function GetBooking(){
             let gEmail = json.bookingitems[i].email;
             let gPax = json.bookingitems[i].pax;
             let gRemarks = json.bookingitems[i].remarks;
-            let gId = "delete" + json.bookingitems[i].id;
+            let gId = json.bookingitems[i].id;
+            let btnId = "delete" + gId;
 
             let row = bookingNameList.insertRow(bookingNameList.rows.length);
             row.insertCell(0).innerHTML = gId;
@@ -29,7 +30,7 @@ function GetBooking(){
             row.insertCell(2).innerHTML = gEmail;
             row.insertCell(3).innerHTML = gPax;
             row.insertCell(4).innerHTML = gRemarks;
-            row.insertCell(5).innerHTML = "<button id='" + gId +"' class='btn btn-danger'>Delete</button>";
+            row.insertCell(5).innerHTML = "<button id='" + btnId +"' class='btn btn-danger'>Delete</button>";
         }
     });
 }
